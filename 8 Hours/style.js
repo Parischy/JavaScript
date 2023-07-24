ef1029de6e2db8fd2d44109800282efbfc37220e
Parischy <Phurichaya.21@mail.kmutt.ac.th>
2023-07-24 22:01:12 +0700

    Update info and 8 hours

---
+++ 8 Hours/style.js
@@ -0,0 +1,7 @@
+document.write("<h1><i>Hello Iris</i></h1>");
+document.write("<p>Lorem ipsum dolor sit amet. Eum laboriosam odit et facere saepe nam adipisci quae et rerum galisum est maxime consequatur?.</p>");
+document.write("<i><b><p>Lorem ipsum dolor sit amet.</p></b></i>");
+
+console.log("Test123");
+
+alert("Halo");
