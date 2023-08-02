f864bf41a1f574f9e3580ba2bb5847071cebabfc
Parischy <Phurichaya.21@mail.kmutt.ac.th>
2023-08-02 23:02:18 +0700

    push

--- edabit/easy2.js
+++ edabit/easy2.js
@@ -77,3 +77,86 @@ function sumOfCubes(nums) {
   return a;
 }
 
+//Sort Numbers in Ascending Order
+function sortNumsAscending(arr) {
+	if (arr == null) {
+    return [];
+  } else {
+		return arr.sort((a, b) => { return a - b });
+	}
+}
+
+/*
+Sort an Array by String Length
+sortByLength(["Google", "Apple", "Microsoft"])
+➞ ["Apple", "Google", "Microsoft"]
+*/
+function sortByLength(arr) {
+	return arr.sort((a, b) => { return a.length - b.length });
+}
+
+/*
+Is the Average of All Elements a Whole Number?
+isAvgWhole([1, 3]) ➞ true;
+isAvgWhole([1, 2, 3, 4]) ➞ false;
+*/
+function isAvgWhole(arr) {
+	let a = 0;
+  let b = arr.length;
+  for (let i = 0; i < b; i++) {
+    a += arr[i];
+  }
+  return Number.isInteger( a / b );
+}
+
+/*
+Recursion: Factorials
+factorial(5) ➞ 120
+*/
+function factorial(int) {
+	if( int > 1 ) {
+    return int * ( factorial ( int - 1) );
+  } else {
+		return 1;
+	}
+}
+
+/*
+Find the Bomb
+bomb("There is a bomb.") ➞ "Duck!!!"
+*/
+function bomb(str) {
+	let a = str.toLowerCase();
+	let b = str.toUpperCase();
+  if ( a.includes("bomb") == true || b.includes("bomb") == true ) {
+    return "Duck!!!";
+  } else {
+		return "There is no bomb, relax.";
+	}
+}
+
+/*
+Absolute Sum
+getAbsSum([2, -1, 4, 8, 10]) ➞ 25
+*/
+function getAbsSum(arr) {
+	let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += Math.abs(arr[i]);
+  }
+  return result;
+}
+
+/*
+Calculate the Mean
+mean([1, 0, 4, 5, 2, 4, 1, 2, 3, 3, 3]) ➞ 2.55
+*/
+function mean(arr) {
+	let a = 0;
+  for (let i = 0; i < arr.length; i++) {
+    a += arr[i];
+  }
+  let sum = a / arr.length;
+  return Number(sum.toFixed(2));
+}
+
