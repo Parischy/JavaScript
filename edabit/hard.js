b89e26c2aebdce15e86cc8f9a65ddce990d87858
Parischy <Phurichaya.21@mail.kmutt.ac.th>
2023-08-13 23:18:45 +0700

    update

--- edabit/hard.js
+++ edabit/hard.js
@@ -15,3 +15,22 @@ function towerHanoi(discs) {
   }
 }
 
+//How Many Days Between Two Dates
+function getDays(date1, date2) {
+	let day = 24 * 60 * 60 * 1000;
+  let milliseconds = Math.abs(date2 - date1);
+  let result = Math.floor(milliseconds / day);
+  return result;
+}
+
+/*
+Basic Calculator
+calculator(2, "+", 2) ➞ 4
+*/
+function calculator(num1, operator, num2) {
+	if (operator == "/" && num2 == 0) {
+    return "Can't divide by 0!"
+  }
+  return eval(num1 + operator + num2)
+}
+
